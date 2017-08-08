6c65009b1dff1c5d3198dd3fc84afcde74449ea0
Gretchen <gretchonian@hotmail.com>
2017-08-08 23:05:08 +0000

    First Attempt

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,23 @@
+class GamesController < ApplicationController
+
+  
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = current_user.games.create(game_params)
+
+  def join
+    #update current_game.update_attributes(merge:user)???
+  end
+
+  private
+
+  # def game_params
+  #   params.require(:game).permit(:current_user)
+  # end
+
+
+
+end
---
+++ spec/controllers/games_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe GamesController, type: :controller do
+    # describe "games#new action" do
+    #   it "should require users to be logged in" do
+    #     get :new
+    #     expect(response).to redirect to ...
+    #   end
+    #   it "should successfully show the form" do
+    #     user = FactoryGirl.create(:user)
+    #     sign_in user
+
+    #     get :new
+    #     expect(response).to have_http_status(:success)
+    #   end
+    # end
+
+    # describe "games#create action" do
+    #   it "should require users to be logged in" do
+    #     post :create, params: {}
+    #     expect(response).to ...
+    #   end
+
+    #   it "should successfully create a new game" do
+    #     user = FactoryGirl.create(:user)
+    #     sign_in user
+
+    #     post :create, parmas {}
+    #     expect(response).to ...
+    #   end
+    # end
+
+end
