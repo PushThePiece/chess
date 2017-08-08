fccd945792d056e63164546006644e316dc4665e
Daniel Strimpel <dastrimpel@gmail.com>
2017-08-08 21:11:54 +0000

    Did more, still haven't run any code

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,38 +2,35 @@ class Piece < ApplicationRecord
   
   attr_accessor :x, :y
 
-  def initialize(loc_x, loc_y)
+  def initialize(loc_x, loc_y, game)
     @x = loc_x
     @y = loc_y
+    @game = game
   end
 
   def is_obstructed?(dest)
+    #need to check its not adjacent
+
     if is_horizontal?(dest)
-      range = (x+1...dest.x)
+      range = (x < dest.x ? (x+1...dest.x) :(dest.x+1...x))
       range.each do |inter_x|
         return true if game.is_occupied?(inter_x, y)
       end
       return false
     elsif is_vertical?(dest)
-      range = (y+1...dest.y)
+      range = (y < dest.y ? (y+1...dest.y) :(dest.y+1...y))
       range.each do |inter_y|
         return true if game.is_occupied?(x, inter_y)
       end
       return false
     elsif is_diagonal?(dest)
-      if dest.x - x == dest.y - y
-        range = (1...dest.x)
-        range.each do |inter_diag|
-          return true if game.is_occupied?(x+inter_diag, y+inter_diag)
-        end
-        return false
-      elsif dest.x - x == (-1)*(dest.y - y)
-        range = (1...dest.x)
-        range.each do |inter_diag|
-          return true if game.is_occupied?(x+inter_diag, y-inter_diag)
-        end
-        return false
+      range_x = ( (x < dest.x ? (1...dest.x - x) : (1...x - dest.x) ).to_a
+      range_y = ( (y < dest.y ? (1...dest.y - y) : (1...y - dest.y) ).to_a
+      
+      range_x.size.times do |i|
+        return true if game.is_occupied?(x+range_x[i], y+range_y[i])
       end
+      return false
     else
       puts "Invalid destination square in is_obstructed?"
       return nil
@@ -54,4 +51,6 @@ class Piece < ApplicationRecord
     return false
   end
 
+  private
+    attr_accessor :game
 end
