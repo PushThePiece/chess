3ce300814699b7f8ec8a93dd6e7b0feda1066db8
Daniel Strimpel <dastrimpel@gmail.com>
2017-08-09 14:58:36 +0000

    Added small test script in main dir, temporary until we write proper tests

---
+++ test_script.rb
@@ -0,0 +1,16 @@
+
+# There is a piece at 4,4
+
+g = Game.new
+
+p = Piece.new(3,3,g)
+puts p.is_obstructed?(3,8)
+
+q = Piece.new(4,8,g)
+puts q.is_obstructed?(4,6)
+
+r = Piece.new(1,1,g)
+puts r.is_obstructed?(5,5)
+
+t = Piece.new(3,3,g)
+puts t.is_obstructed?(8,3)
