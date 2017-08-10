74874eca6df7ab5f2b429963d9a4715f19566fb1
Roger Wang <blumoo@gmail.com>
2017-08-10 22:55:50 +0000

    added chess pieces to board

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ApplicationRecord
   has_many :pieces
+
+  def populate_game!
+    Rook.create(game_id: id, current_location: A1)
+    Rook.create(game_id: id, current_location: H1)
+
+    end
+
+  end
 end
