119d73afe17ed18a81ef57ad86920ea79c8ba61c
Gretchen <gretchonian@hotmail.com>
2017-08-10 23:01:00 +0000

    Update Method and TDD

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,8 +1,17 @@
 class Piece < ApplicationRecord
   belongs_to :game
-  self.inheritance_column :types
+  self.inheritance_column 
 
   def self.types
     %w(Pawn Knight Rook King Queen Bishop)
   end
+  
+  def initialize(loc_x, loc_y, game)
+    super()
+    @x = loc_x
+    @y = loc_y
+    @game = game
+  end
+  
+
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  it "should successfully test the is_move_valid? method" do
+    g = Game.new
+    k = King.new(4,3,g) 
+    expect(k.is_move_valid?(3, 2)).to be(false)
+    expect(k.is_move_valid?(2, 2)).to be(false)
+    expect(k.is_move_valid?(3, 3)).to be(true)
+    expect(k.is_move_valid?(4, 4)).to be(true)
+    expect(k.is_move_valid?(4, 6)).to be(false)
+  end
 
 end
