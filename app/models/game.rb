8b258fa5e9dcc0b847b50ff4e3aeadaa4a61e478
Roger Wang <blumoo@gmail.com>
2017-08-14 12:37:23 +0000

    tested populate game method, added individual unicode images for each piece, and set up controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,42 +11,46 @@ class Game < ApplicationRecord
     return Piece.where(:x => x, :y => y, :game_id => id).first
   end
 
-
-  private
+  def self.create_game(params)
+    game = Game.create(params)
+    game.populate_game!
+    game
+  end
   
   def populate_game!
     #white pieces
-    (1..8).each do |i|
-      Pawn.create(self: id, x: i, y: 2, color: 'white')
+    (1..8).each do |position|
+      Pawn.create(game_id: id, x: position, y: 2, color: 'white')
     end
 
-    Rook.create(self: id, x: 1, y: 1, color: 'white')
-    Rook.create(self: id, x: 8, y: 1, color: 'white')
+    Rook.create(game_id: id, x: 1, y: 1, color: 'white')
+    Rook.create(game_id: id, x: 8, y: 1, color: 'white')
 
-    Knight.create(self: id, x: 2, y: 1, color: 'white')
-    Knight.create(self: id, x: 7, y: 1, color: 'white')
+    Knight.create(game_id: id, x: 2, y: 1, color: 'white')
+    Knight.create(game_id: id, x: 7, y: 1, color: 'white')
 
-    Bishop.create(self: id, x: 3, y: 1, color: 'white')
-    Bishop.create(self: id, x: 6, y: 1, color: 'white')
+    Bishop.create(game_id: id, x: 3, y: 1, color: 'white')
+    Bishop.create(game_id: id, x: 6, y: 1, color: 'white')
 
-    King.create(self: id, x: 4, y: 1, color: 'white')
-    Queen.create(self: id, x: 5, y: 1, color: 'white')
+    King.create(game_id: id, x: 4, y: 1, color: 'white')
+    Queen.create(game_id: id, x: 5, y: 1, color: 'white')
 
     #black pieces
-    (1..8).each do |i|
-      Pawn.create(self: id, x: i, y: 7, color: 'black')
+    (1..8).each do |position|
+      Pawn.create(game_id: id, x: position, y: 7, color: 'black')
     end
 
-    Rook.create(self: id, x: 1, y: 8, color: 'black')
-    Rook.create(self: id, x: 8, y: 8, color: 'black')
+    Rook.create(game_id: id, x: 1, y: 8, color: 'black')
+    Rook.create(game_id: id, x: 8, y: 8, color: 'black')
 
-    Knight.create(self: id, x: 2, y: 8, color: 'black')
-    Knight.create(self: id, x: 7, y: 8, color: 'black')
+    Knight.create(game_id: id, x: 2, y: 8, color: 'black')
+    Knight.create(game_id: id, x: 7, y: 8, color: 'black')
 
-    Bishop.create(self: id, x: 3, y: 8, color: 'black')
-    Bishop.create(self: id, x: 6, y: 8, color: 'black')
+    Bishop.create(game_id: id, x: 3, y: 8, color: 'black')
+    Bishop.create(game_id: id, x: 6, y: 8, color: 'black')
 
-    King.create(self: id, x: 4, y: 8, color: 'black')
-    Queen.create(self: id, x: 5, y: 8, color: 'black')
+    King.create(game_id: id, x: 4, y: 8, color: 'black')
+    Queen.create(game_id: id, x: 5, y: 8, color: 'black')
   end
+
 end
--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,7 @@
 class King < Piece
+
+  def image_piece
+    color == 'white' ? '&#9812' : '&#9818'
+  end
+
 end
