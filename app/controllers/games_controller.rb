6f187b9e78e3ffc321a34991ca4f443b87941e71
Gretchen <gretchonian@hotmail.com>
2017-08-14 21:43:03 +0000

    Update for create method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,39 +1,35 @@
 class GamesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :join, :show]
-
-  helper_method :game
+  before_action :authenticate_user!
+  # helper_method :game
 
   def index
 
   end
 
   def show
-    # @game = Game.find(params[:id])
+    @game = Game.find(params[:id])
   end
 
-  
-  # def new
-  #   @game = Game.new
-  # end
+  def new
+    @game = Game.new
+  end
 
-  # def create
-  #   @game = current_user.games.create(game_params)
-  # end
+  def create
+    current_user = game_params[:user]
+  end
 
   # def join
   #   #update current_game.update_attributes(merge:user)???
   # end
 
-  # private
-
-  # def game_params
-  #   params.require(:game).permit(:current_user)
-  # end
-
   private
 
-  def game
-    @game ||= Game.where(id: params[:id]).last
+  def game_params
+    params.require(:game).permit(:user)
   end
+
+  # def game
+  #   @game ||= Game.where(id: params[:id]).last
+  # end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,56 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-  it "should successfully determine if piece is obstructed in a horizontal path" do
-    g = Game.new
 
+        it "should successfully determine if piece is obstructed in a horizontal path" do
+            g = Game.new
+            p = Piece.create(x: 3, y: 3, game: g)
+            q = Piece.create(x: 6, y: 3, game: g)
+            expect(p.is_obstructed?(7,3)).to be(true)
+            expect(p.is_obstructed?(5,3)).to be(false)
+        end
 
-    p = Piece.create(x: 3, y: 3, game: g)
-    q = Piece.create(x: 6, y: 3, game: g)
-    
-    expect(p.is_obstructed?(7,3)).to be(true)
+        it "should successfully determine if piece is obstructed in a vertical path" do
+            g = Game.new
+            p = Piece.create(x: 3, y: 3, game: g)
+            q = Piece.create(x: 3, y: 6, game: g)
+            expect(p.is_obstructed?(3,7)).to be(true)
+            expect(p.is_obstructed?(3,5)).to be(false)
+        end
 
-    expect(p.is_obstructed?(5,3)).to be(false)
+        it "should successfully determine if piece is obstructed in a diagonal path" do
+            g = Game.new
+            p = Piece.create(x: 3, y: 3, game: g)
+            q = Piece.create(x: 6, y: 6, game: g)
+            expect(p.is_obstructed?(8,8)).to be(true)
+            expect(p.is_obstructed?(5,5)).to be(false)
+        end
 
-  end
+    describe "games#new action" do
+        it "should require users to be logged in" do
+        get :new
+        expect(response).to redirect_to new_user_session_path
+      end
+      it "should successfully show 'create a new game' form" do
+        user = FactoryGirl.create(:user)
+        sign_in user
 
-  it "should successfully determine if piece is obstructed in a vertical path" do
-    g = Game.new
-
-    p = Piece.create(x: 3, y: 3, game: g)
-    q = Piece.create(x: 3, y: 6, game: g)
-    
-    expect(p.is_obstructed?(3,7)).to be(true)
-    expect(p.is_obstructed?(3,5)).to be(false)
-
-  end
-
-  it "should successfully determine if piece is obstructed in a diagonal path" do
-    g = Game.new
-
-    p = Piece.create(x: 3, y: 3, game: g)
-    q = Piece.create(x: 6, y: 6, game: g)
-    
-    expect(p.is_obstructed?(8,8)).to be(true)
-
-    expect(p.is_obstructed?(5,5)).to be(false)
-
-  end
-
-  
-    # describe "games#new action" do
-    #   it "should require users to be logged in" do
-    #     get :new
-    #     expect(response).to redirect_to new_user_session_path
-    #   end
-    #   it "should successfully show the form" do
-    #     user = FactoryGirl.create(:user)
-    #     sign_in user
-
-    #     get :new
-    #     expect(response).to have_http_status(:success)
-    #   end
-    # end
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+    end
 
     # describe "games#create action" do
     #   it "should require users to be logged in" do
