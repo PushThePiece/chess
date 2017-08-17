df8d7eaddcf5be10ca12f516e5df9f9adf4e2671
Gretchen <gretchonian@hotmail.com>
2017-08-17 22:14:02 +0000

    Rook valid_move?

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -4,4 +4,9 @@ class Rook < Piece
     color == 'white' ? '&#9814' : '&#9820'
   end
 
+  def valid_move?(x, y)
+    #assumes valid_move? in piece.rb has been verified prior to this step
+    is_horizontal?(x, y) || is_vertical?(x, y)
+  end
+
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :Knight do
-  it "should successfully determine if a move is valid" do
+  it "should successfully determine if the Knight's move is valid" do
     g = Game.create
     k = Knight.create(x: 3, y: 3, game: g)
     expect(k.valid_move?(4,5)).to be(true)
     expect(k.valid_move?(5,5)).to be(false)
+  end
+end
+
+RSpec.describe Piece, type: :Rook do
+  it "should successfully determine if the Rook's move is valid" do
+    g = Game.create
+    r = Rook.create(x: 1, y: 1, game: g)
+    expect(r.valid_move?(2,2)).to be(false)
+    expect(r.valid_move?(1,5)).to be(true)
+    expect(r.valid_move?(3,1)).to be(true)
+    expect(r.valid_move?(5,5)).to be(false)
   end
 end
