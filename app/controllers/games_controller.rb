ff7c294355dfcd426703030ba4fc167f590d0cdc
Gretchen <gretchonian@hotmail.com>
2017-08-17 14:34:05 +0000

    Create Woring w/Rspec

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,9 @@
 class GamesController < ApplicationController
-
   before_action :authenticate_user!
-  # helper_method :game
+  helper_method :game
 
   def index
-    # @game = Game.all
+    @game = Game.all
   end
 
   def new
@@ -12,15 +11,18 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = Game.create()  
+    @game = Game.create(black: current_user, white: current_user)
+    # Game.create(black: User.last, white: User.last) works in console  
   end
 
-  def join
-    if @game.white_user_id.present?
-      black_user_id = user.id
-    else
-      white_user_id = user.id
-    end
+  def update
+    @game = Game.find_by_id(params[:id])
+    @game.update_attributes(game_params.merge(white: current_user))
+    # if @game.white_user_id.present?
+    #   black_user_id = user.id
+    # else
+    #   white_user_id = user.id
+    # end
     if @game.valid?
       @game.populate_game!
       redirect_to games_path
@@ -37,11 +39,10 @@ class GamesController < ApplicationController
   private
 
   def game_params
-    # params.require(:game).permit(:black)
     params.require(:game).permit(:black, :white)
   end
 
-  # def game
-  #   @game ||= Game.where(id: params[:id]).last
-  # end
+  def game
+    @game ||= Game.where(id: params[:id]).last
+  end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
 
 
-    # it "should successfully test the is_move_valid? method" do
-    #   g = Game.new
-    #   k = King.create(x: 4, y: 3, game: g) 
-    #   expect(k.is_move_valid?(3, 2)).to be(true)
-    #   expect(k.is_move_valid?(2, 2)).to be(false)
-    #   expect(k.is_move_valid?(3, 3)).to be(true)
-    #   expect(k.is_move_valid?(4, 4)).to be(true)
-    #   expect(k.is_move_valid?(4, 6)).to be(false)
-    #   expect(k.is_move_valid?(5, 4)).to be(true)
-    # end
+    it "should successfully test the is_move_valid? method" do
+      g = Game.new
+      k = King.create(x: 4, y: 3, game: g) 
+      expect(k.is_move_valid?(3, 2)).to be(true)
+      expect(k.is_move_valid?(2, 2)).to be(false)
+      expect(k.is_move_valid?(3, 3)).to be(true)
+      expect(k.is_move_valid?(4, 4)).to be(true)
+      expect(k.is_move_valid?(4, 6)).to be(false)
+      expect(k.is_move_valid?(5, 4)).to be(true)
+    end
 
     it "should successfully determine if piece is obstructed in a horizontal path" do
         g = Game.new
@@ -39,17 +39,16 @@ RSpec.describe GamesController, type: :controller do
     end
 
   describe "games#index" do
-    # it "should successfully show the games/index page" do
-    #   get :index
-    #   expect(response).to have_http_status(:success)
-    # end
-  end
+    it "should successfully show the games/index page" do
+      # get :index
+      # expect(response).to have_http_status(:success)
+    end
 
-    # it "should successfully display the pieces on the board" do
-    #   g = Game.create(:white_user_id => 1, :black_user_id => 1)
-    #   expect(g.populate_game!)
-    #   end
-    # end
+    it "should successfully display the pieces on the board" do
+      # g = Game.create(:white => 1, :black => 1)
+      # expect(g.populate_game!)
+    end
+  end
 
   describe "games#new action" do
     it "should require users to be logged in" do
@@ -74,20 +73,27 @@ RSpec.describe GamesController, type: :controller do
     it "should successfully create a new game" do
       user1 = FactoryGirl.create(:user)
       sign_in user1
-      # g = Game.create(black: user, white: user) rails console this works
       post :create, params: { game: {black: user1, white: user1} }
     
-      # game = Game.last
-      # user2 = FactoryGirl.create(:user)
-      # sign_in user2
+      game = Game.last
+      
+      expect(game.white).to eq(user1)
+      expect(game.black).to eq(user1)
+      
+    end
+
+    it "should allow a second player to join a game" do
+      user1 = FactoryGirl.create(:user)
+      sign_in user1
+      game = FactoryGirl.create(:game)
+      
+      user2 = FactoryGirl.create(:user)
+      sign_in user2
+
+      post :update, params: { id: game.id {white: user2 } }
       # expect(game).to eq(Game.last)
-      # expect(game.white_user_id).to eq(nil)
-      # expect(game.black).to eq(user2)
       # user2 = FactoryGirl.create(:user)
       # sign_in user2
-
-      #post :
-
       # expect(response).to redirect_to game_path
     end
   end
