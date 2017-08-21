6ea4eb28c183c6451bb09f60dca86eed6509e239
Daniel Strimpel <dastrimpel@gmail.com>
2017-08-21 16:14:49 +0000

    Cleaned up unnecessary code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,23 +1,20 @@
 class GamesController < ApplicationController
   before_action :authenticate_user!
-  helper_method :game
   
   def index
     @game = Game.all
   end
 
   def create
-    @game = Game.create(black: current_user, white: current_user)
-    redirect_to game_path(@game)
+    @game = Game.create
+    redirect_to games_path
   end
 
   def update
-    new_player = current_user
-    @game = current_game.update(game_params.merge(white: new_player)) 
+
   end
 
   def show
-    #@game = Game.create
     @game = current_game
   end
 
@@ -26,10 +23,6 @@ class GamesController < ApplicationController
 
   def current_game
     Game.where(id: params[:id]).last
-  end
-    
-  def game_params
-    params.require(:game).permit(:white, :black)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#index'
+  root 'games#index'
   resources :games
   resources :pieces, :only => [:show, :update]
 end
