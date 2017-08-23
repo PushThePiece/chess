01a819f398446b3eeaaf592056de6efe2fa3dee4
Daniel Strimpel <dastrimpel@gmail.com>
2017-08-23 18:42:50 +0000

    Added valid_move? method to Queen model

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -4,4 +4,8 @@ class Queen < Piece
     color == 'white' ? '&#9813' : '&#9819'
   end
 
+  def valid_move?(dest_x,dest_y)
+    is_horizontal?(dest_x,dest_y) || is_vertical?(dest_x,dest_y) || is_diagonal?(dest_x,dest_y)
+  end
+
 end
