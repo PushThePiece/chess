bc1c64030b93cdff2199bc71fd5e97e31283bf15
Daniel Strimpel <dastrimpel@gmail.com>
2017-08-25 16:45:01 +0000

    Refactored, now test for black castling not passing.

--- app/models/king.rb
+++ app/models/king.rb
@@ -4,33 +4,26 @@ class King < Piece
     is_adjacent?(dest_x, dest_y)
   end
 
-  def castle_kingside!
+  def can_castle_kingside
     return false if has_moved?
+    corner_piece = color == "white" ? game.get_piece_at(8,1) : game.get_piece_at(8,8)
+    return false if corner_piece.type != "Rook" || corner_piece.has_moved?
+    return false if game.is_occupied?(7,1) || game.is_occupied?(6,1)
+    return true
+  end
 
+  def castle_kingside!
+    return false if !can_castle_kingside
+    corner_piece = color == "white" ? game.get_piece_at(8,1) : game.get_piece_at(8,8)
     if color == "white" 
-      corner_piece = game.get_piece_at(8,1)
-      if corner_piece.type == "Rook" && corner_piece.has_moved? == false
-        if !game.is_occupied?(7,1) && !game.is_occupied?(6,1)
-          #do castle
-          update_attributes(:x => 7,:y => 1, :has_moved? => true)
-          corner_piece.update_attributes!(:x => 6, :y => 1, :has_moved? => true)
-          return true
-        end
-      end
-
+      update_attributes(:x => 7,:y => 1, :has_moved? => true)
+      corner_piece.update_attributes!(:x => 6, :y => 1, :has_moved? => true)
+      return true
     elsif color == "black"
-      corner_piece = game.get_piece_at(8,8)
-      if corner_piece.type == "Rook" && corner_piece.has_moved? == false
-        if !game.is_occupied?(7,8) && !game.is_occupied?(6,8)
-          #do castle
-          update_attributes(:x => 7,:y => 8, :has_moved? => true)
-          corner_piece.update_attributes!(:x => 6, :y => 8, :has_moved? => true)
-          return true
-        end
-      end
+      update_attributes(:x => 7,:y => 8, :has_moved? => true)
+      corner_piece.update_attributes!(:x => 6, :y => 8, :has_moved? => true)
+      return true
     end
-
-    return false
   end
 
   def unicode_point
