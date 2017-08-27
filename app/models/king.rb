b47562731778c407fd58ee53db5897aeb2364298
Daniel Strimpel <dastrimpel@gmail.com>
2017-08-27 16:15:34 +0000

    Further clean up as per Code Climate's complaints. More DRY now.

--- app/models/king.rb
+++ app/models/king.rb
@@ -5,39 +5,35 @@ class King < Piece
   end
 
   def can_castle?(corner_pos_x, corner_pos_y)
+
     return false if has_moved? || corner_pos_y != y
+
     piece = game.get_piece_at(corner_pos_x,corner_pos_y)
     return false if piece.nil? || piece.type != "Rook" || piece.has_moved?
+
     obstr_range = (piece.x == 8) ? [6, 7] : [2,3,4]
     obstr_range.each { |sq| return false if game.is_occupied?(sq, y) }
+    
     return true
   end
 
   def castle!(is_king_side = true)
     corner_pos_x = (is_king_side == true) ? 8 : 1
     corner_pos_y = (color == "white") ? 1 : 8
+
     return false if !can_castle?(corner_pos_x, corner_pos_y)
+
     rook = game.get_piece_at(corner_pos_x, corner_pos_y)
 
+    update_attributes(corner_pos_x, corner_pos_y, :has_moved? => true)
+
     if is_king_side
-      if color == "white"
-        update_attributes(:x => 7,:y => 1, :has_moved? => true)
-        rook.update_attributes!(:x => 6, :y => 1, :has_moved? => true)
-      else #color is black
-        update_attributes(:x => 7,:y => 8, :has_moved? => true)
-        rook.update_attributes!(:x => 6, :y => 1, :has_moved? => true)
-      end
-      return true
-    else #queenside
-      if color == "white"
-        update_attributes(:x => 3,:y => 1, :has_moved? => true)
-        rook.update_attributes!(:x => 4, :y => 1, :has_moved? => true)     
-      else #black
-        update_attributes(:x => 3,:y => 8, :has_moved? => true)
-        corner_piece.update_attributes!(:x => 4, :y => 8, :has_moved? => true)
-      end
-      return true
+      rook.update_attributes!(corner_pos_x - 2, corner_pos_y, :has_moved? => true)
+    else
+      rook.update_attributes!(corner_pos_x + 3, corner_pos_y, :has_moved? => true)
     end
+    
+    return true
   end
 
   def unicode_point
