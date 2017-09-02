851e8e34f696b393397ef5d4f87f93553ed30071
Gretchen <gretchonian@hotmail.com>
2017-09-02 06:09:41 -0700

    Revert "Hooked up controller with move_to backend logic"

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -7,7 +7,7 @@ class PiecesController < ApplicationController
 
   def update
     @piece = Piece.find(params[:id])
-    @piece.move_to!(piece_params["x"].to_i, piece_params["y"].to_i)
+    @piece.update_attributes(piece_params)
     render json: @piece
   end
 
