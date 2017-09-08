7e6ab44ded33609efbf86bf243a12f9f7e7a9237
Gretchen <gretchonian@hotmail.com>
2017-09-08 19:56:13 +0000

    Added Black Player to FactoryGirl

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,8 @@
 FactoryGirl.define do 
-  factory :user, aliases: [:white_player] do
+  factory :user, aliases: [:white_player, :black_player] do
     sequence(:email) do |n|
       "dummyEmail#{n}@gmail.com"
     end
-    # sequence(:email, 'A', aliases: [:black_player]) do |n|
-    #   "dummyEmail#{n}@gmail.com"
-    # end
   password              "secretPassword"
   password_confirmation "secretPassword"
   end
@@ -13,22 +10,6 @@ FactoryGirl.define do
   factory :game do
     white_player 
     turn { white_user_id }
-   
-
-    # factory :update_game do
-    #   black_player 
-    # end
-    
-    # association  :white_player, factory: :user
-    # association  :black_player, factory: :email 
-  # end
-
-#   factory :user do
-#     sequence :email do |n|
-#       "dummyEmail#{n}@gmail.com"
-#     end
-#  password              "secretPassword"
-#   password_confirmation "secretPassword"
-# end
-end
+    black_player 
+  end
 end
