ca67b5a4feabaf25a262de4aeb487314ebbfdff2
Gretchen <gretchonian@hotmail.com>
2017-09-11 14:28:47 +0000

    Activated rspec code

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe GamesController, type: :controller do
 
   describe "games#index" do
     it "should successfully show the games#index page" do
-      # user1 = FactoryGirl.create(:user)
-      # sign_in user1
+      user1 = FactoryGirl.create(:user)
+      sign_in user1
 
-      # get :index
-      # expect(response).to have_http_status(:success)
+      get :index
+      expect(response).to have_http_status(:success)
     end
   end
 
@@ -19,34 +19,34 @@ RSpec.describe GamesController, type: :controller do
     end
 
     it "should successfully create a new game" do
-      # user1 = FactoryGirl.create(:user)
-      # sign_in user1
-      # post :create, params: { game: {white_user_id: user1.id} }
+      user1 = FactoryGirl.create(:user)
+      sign_in user1
+      post :create, params: { game: {white_player: user1} }
     
-      # game = Game.last
+      game = Game.last
       
-      # expect(game.white_user_id).to eq(user1.id)
-      # expect(response).to redirect_to game_path(game.id)
+      expect(game.white_user_id).to eq(user1.id)
+      expect(response).to redirect_to game_path(game.id)
     end
   end
 
   describe "games#update action" do
     it "should allow a second player to join a game" do 
-      # game = FactoryGirl.create(:game)
+      game = FactoryGirl.create(:game)
 
-      # user2 = FactoryGirl.create(:user)
-      # sign_in user2
-      # put :update, params: { id: game.id, game: { black_user_id: user2.id } }
+      user2 = FactoryGirl.create(:user)
+      sign_in user2
+      put :update, params: { id: game.id, game: { black_player: user2 } }
 
-      # game = Game.last
-      # expect(game.black_user_id).to eq(user2.id)
-      # expect(response).to redirect_to game_path(game.id)
+      game = Game.last
+      expect(game.black_user_id).to eq(user2.id)
+      expect(response).to redirect_to game_path(game.id)
     end
   end
 
   describe "games#show action" do
       it "should successfully display the pieces on the board" do
-      g = Game.create(:white_user_id => 1, :black_user_id => 1)
+      g = Game.create(:white_user_id => 1, :black_user_id => 2)
       expect(g.populate_game!)
     end
   end
